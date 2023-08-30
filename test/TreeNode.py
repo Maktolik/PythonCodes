00b4c78258ff2ffa064ce904cc694982441772da
Maktolik <maktolik97@gmail.com>
2023-08-30 18:15:38 +0300

    binar tree

---
+++ test/TreeNode.py
@@ -0,0 +1,37 @@
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def pre_order(node):
+    if node:
+        print(node.value)
+        pre_order(node.left)
+        pre_order(node.right)
+
+
+def post_order(node):
+    if node:
+        post_order(node.left)
+        post_order(node.right)
+        print(node.value)
+
+
+def in_order(node):
+    if node:
+        in_order(node.left)
+        print(node.value)
+        in_order(node.right)
+
+
+tree = TreeNode(1)
+tree.left = TreeNode(2)
+tree.right = TreeNode(3)
+tree.left.left = TreeNode(4)
+tree.left.right = TreeNode(5)
+
+# in_order(tree)
+
+
